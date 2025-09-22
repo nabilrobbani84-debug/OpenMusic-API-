6bf0757e867498a9a977b56b4a0cc2c852236081
nabilrobbani84 <nabilrobbani6@gmail.com>
2025-09-22 21:19:44 +0700

    albums\handler

--- src/api/albums/handler.js
+++ src/api/albums/handler.js
@@ -1,26 +1,50 @@
 // src/api/albums/handler.js
 import autoBind from 'auto-bind';
+import InvariantError from '../../exceptions/InvariantError.js';
+import NotFoundError from '../../exceptions/NotFoundError.js';
 
 class AlbumsHandler {
   constructor(service, validator) {
     this._service = service;
     this._validator = validator;
+
     autoBind(this);
   }
 
   async postAlbumHandler(request, h) {
-    this._validator.validateAlbumPayload(request.payload);
-    const { name, year } = request.payload;
-    const albumId = await this._service.addAlbum({ name, year });
-    const response = h.response({
-      status: 'success',
-      data: { albumId },
-    });
-    response.code(201);
-    return response;
+    try {
+      this._validator.validateAlbumPayload(request.payload);
+      const { name, year } = request.payload;
+      const albumId = await this._service.addAlbum({ name, year });
+      const response = h.response({
+        status: 'success',
+        data: {
+          albumId,
+        },
+      });
+      response.code(201);
+      return response;
+    } catch (error) {
+      if (error instanceof InvariantError) {
+        const response = h.response({
+          status: 'fail',
+          message: error.message,
+        });
+        response.code(400);
+        return response;
+      }
+      // Handle server errors
+      const response = h.response({
+        status: 'error',
+        message: 'Maaf, terjadi kegagalan pada server kami.',
+      });
+      response.code(500);
+      console.error(error);
+      return response;
+    }
   }
 
-  async getAlbumsHandler(request, h) {
+  async getAlbumsHandler() {
     const albums = await this._service.getAlbums();
     return {
       status: 'success',
@@ -31,31 +55,66 @@ class AlbumsHandler {
   }
 
   async getAlbumByIdHandler(request, h) {
-    const { id } = request.params;
-    const album = await this._service.getAlbumById(id);
-    return {
-      status: 'success',
-      data: { album },
-    };
+    try {
+      const { id } = request.params;
+      const album = await this._service.getAlbumById(id);
+      return {
+        status: 'success',
+        data: {
+          album,
+        },
+      };
+    } catch (error) {
+      if (error instanceof NotFoundError) {
+        const response = h.response({
+          status: 'fail',
+          message: error.message,
+        });
+        response.code(404);
+        return response;
+      }
+    }
   }
 
   async putAlbumByIdHandler(request, h) {
-    this._validator.validateAlbumPayload(request.payload);
-    const { id } = request.params;
-    await this._service.editAlbumById(id, request.payload);
-    return {
-      status: 'success',
-      message: 'Album berhasil diperbarui',
-    };
+    try {
+      this._validator.validateAlbumPayload(request.payload);
+      const { id } = request.params;
+      await this._service.editAlbumById(id, request.payload);
+      return {
+        status: 'success',
+        message: 'Album berhasil diperbarui',
+      };
+    } catch (error) {
+      if (error instanceof InvariantError) {
+        const response = h.response({
+          status: 'fail',
+          message: error.message,
+        });
+        response.code(400);
+        return response;
+      }
+    }
   }
 
   async deleteAlbumByIdHandler(request, h) {
-    const { id } = request.params;
-    await this._service.deleteAlbumById(id);
-    return {
-      status: 'success',
-      message: 'Album berhasil dihapus',
-    };
+    try {
+      const { id } = request.params;
+      await this._service.deleteAlbumById(id);
+      return {
+        status: 'success',
+        message: 'Album berhasil dihapus',
+      };
+    } catch (error) {
+      if (error instanceof NotFoundError) {
+        const response = h.response({
+          status: 'fail',
+          message: error.message,
+        });
+        response.code(404);
+        return response;
+      }
+    }
   }
 }
 
